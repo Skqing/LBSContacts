59695c3b30e385d19272810d1e62b05d313f404f
DolphinBoy <dolphinboyo@gmail.com>
2013-02-25 23:31:44 +0800

    学习项目配置文件内容

--- app/Conf/config.php
+++ app/Conf/config.php
@@ -1,5 +1,5 @@
 <?php
-return array(
+$personality_config = array(
     'APP_NAME'        => 'LBSContacts',  //项目名称
     'APP_TITLENAME'   => 'LBS通讯录',  //项目标题名称
 	//'配置项'=>'配置值'
@@ -7,8 +7,19 @@ return array(
     //'URL_PATHINFO_MODEL'=>2, //
     //'URL_PATHINFO_DEPR'=>'-', // 更改PATHINFO参数分隔符
 
-    'DB_DSN'          => 'mysql://root@localhost:3306/thinkphp',  //数据库配置信息
+    'APP_DEBUG'       => true,  //开启调试模式
 
-    //'APP_DEBUG'       => true,  //开启调试模式
+    /*
+    'SHOW_RUN_TIME'=>true,  //运行时间显示
+    'SHOW_ADV_TIME'=>true,//显示详细的运行时间
+    'SHOW_DB_TIMES'=>true,//显示数据库的操作次数
+    'SHOW_CACHE_TIMES'=>true,//显示缓存操作次数
+    'SHOW_USE_MEM'=>true,//显示内存开销
+    */
+
+//    'LOG_RECORD'=>true,//开启了日志记录
+//    'LOG_RECORD_LEVEL'=>array('EMERG','ALERT','ERR'),
 );
+$global_config = include './config.inc.php';
+return array_merge($personality_config,$global_config);
 ?>
